6d479a2a63914ae2b890a443c39cd5ae6254d884
Benjamin Owusu <owusubenjamin446@gmail.com>
2026-03-26 13:36:56 +0000

    Performance Optimization

---
+++ script.js
@@ -0,0 +1,16 @@
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("navLinks");
+
+hamburger.addEventListener("click", () => {
+    navLinks.style.display =
+        navLinks.style.display === "flex" ? "none" : "flex";
+});
+
+// Adding Debounce function to Improve Interactivity
+function debounce(func, delay){
+    let timeout;
+    return function(){
+        clearTimeout(timeout);
+        timeout = setTimeout(() => func.apply(this, arguments), delay);
+    };
+}
